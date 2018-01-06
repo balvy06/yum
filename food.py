105c4de5976f3df03f8c9cebc176e1b42e502cd2
Ben Alvarado <balvy06@gmail.com>
2018-01-05 22:49:05 -0800

    Added comments for nutrients
    Looked at Apples, raw, with skin. Extracted key details.

--- food.py
+++ food.py
@@ -1,3 +1,45 @@
 # Info from the USDA (United States Department of Agriculture) National Nutrient Database for Standard Reference
 # 
 # NDB 09003 Apples, raw, with skin
+
+
+# Water
+# Unit: g
+
+# Energy
+# Unit: kcal
+
+# Protein
+# Unit: g
+
+# Total lipid (fat)
+# Unit: g
+
+# Carbohydrate (by difference)
+# Unit: g
+
+# Fiber (total dietary)
+# Unit: g
+
+# Sugars
+# Unit: g
+
+
+# Minerals
+# Mineral: Calcium (Ca), Iron (Fe), Magnesium (Mg), Phosphorus (P), Potassium (K), Sodium (Na), Zinc (Zn)
+# Unit: mg
+
+# Vitamins
+# Vitamin: Vitamin C, Thiamin, Riboflavin, Niacin, Vitamin B-6, Folate, Vitamin B-12, Vitamin A (RAE), Vitamin A (IU), Vitamin E (alpha-tocopherol), Vitamin D (D2 + D3), Vitamin D (IU), Vitamin K (phylloquinone)
+# Unit: mg, microg
+
+# Lipids
+# Lipid: Fatty acids, total saturated; Fatty acids, total monounsaturated; Fatty acids, total polyunsaturated; Fatty acids, total trans; Cholesterol
+# Unit: g, mg
+
+
+# Caffeine
+# Unit: mg 
+
+
+# Amounts
